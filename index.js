b181770dc402cf5c07b7e6b6e3610f75274f1515
amit-077 <amitkamat918@gmail.com>
2024-07-24 23:53:30 +0530

    new changes

--- index.js
+++ index.js
@@ -15,11 +15,12 @@ app.post("/analyze", async (req, res) => {
   try {
     let { code } = req.body;
     const answer = await model.generateContent(
-      `Analyze the time complexity and space complexity of below code. In response, just give the time and space complexity and nothing else. And give precise time and space complexity. Like if it's O(2N) don't give O(N), give O(2N). Just provide response in plain text, no markdown syntax, not any special characters nothing, just plain text.${code}`
+      `Analyze the time complexity and space complexity of below code. In response, just give the time and space complexity and nothing else. And give precise time and space complexity. Like if it's O(2N) don't give O(N), give O(2N). Don't use any kind of markdown syntax in answer. Follow the below given template in single quotes in response. Time Complexity: ____ Space Complexity: ____. Don't include any single, double quotes in respone. ${code}`
     );
     let complexity = answer.response.text();
     const timeMatch = complexity.match(/Time complexity:\s*([^\s]+)/i);
     const spaceMatch = complexity.match(/Space complexity:\s*([^\s]+)/i);
+    console.log(complexity);
 
     const result = {
       time: timeMatch ? timeMatch[1] : "Unknown",
